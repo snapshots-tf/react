bda2508c915561225b9ae18daa5d11166abf115f
Viggo Jonasson <aethezjonasson@gmail.com>
2021-08-19 17:59:33 +0200

    fix: oops forgot to add to mobile sidebar

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -181,7 +181,7 @@ export default function Sidenav({
                                                         item.current
                                                             ? 'bg-gray-900 text-white'
                                                             : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                        'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                                                        'group flex items-center px-2 py-2 text-base font-medium rounded-md navbar-item'
                                                     )}
                                                 >
                                                     <item.icon
