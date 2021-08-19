2355b3bcbb1650a1c3a58fc7200417ad49d60720
Viggo Jonasson <aethezjonasson@gmail.com>
2021-08-19 18:33:57 +0200

    destory cookies on logout

--- pages/auth/logout.tsx
+++ pages/auth/logout.tsx
@@ -1,13 +1,12 @@
 import { GetServerSideProps } from 'next';
+import nookies from 'nookies';
 
 export default function Logout() {
     return <span></span>;
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
-    req.headers.cookie = undefined;
-    // @ts-ignore
-    req.cookies = undefined;
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+    nookies.destroy(ctx, 'snptf_user');
 
     return {
         redirect: {
