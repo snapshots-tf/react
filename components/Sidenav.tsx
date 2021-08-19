7fc9ab126423e0c82f089c9d5f862a52b1f34ea7
Viggo Jonasson <aethezjonasson@gmail.com>
2021-08-19 17:58:51 +0200

    make sidenav icons move :D

--- components/Sidenav.tsx
+++ components/Sidenav.tsx
@@ -267,7 +267,7 @@ export default function Sidenav({
                                                     item.current
                                                         ? 'bg-gray-900 text-white'
                                                         : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                                                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
+                                                    'group flex items-center px-2 py-2 text-sm font-medium rounded-md navbar-item'
                                                 )}
                                             >
                                                 <item.icon
